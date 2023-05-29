9d853d456b91369c04c97a587da69daf941b90b2
Manish Akula <manishakulaa@gmail.com>
2023-05-29 04:42:46 -0500

    Added View Itinerary and Create Itinerary Functionality

---
+++ src/views/CreateItinerary.vue
@@ -0,0 +1,167 @@
+<script setup>
+import { onMounted, ref } from "vue";
+import { useRouter, useRoute } from "vue-router";
+import {fileFromPath} from 'formdata-node/file-from-path'
+import IngredientServices from "../services/IngredientServices.js";
+import RecipeIngredientServices from "../services/RecipeIngredientServices";
+import RecipeStepServices from "../services/RecipeStepServices";
+import ItineraryServices from "../services/ItineraryServices";
+
+const route = useRoute();
+const router = useRouter();
+
+var itinerary = ref({});
+let isAddItinerary = ref(true);
+let isEditItinerary = ref(false);
+const itineraryDay = ref([]);
+const site = ref([]);
+const hotel = ref([]);
+
+const snackbar = ref({
+  value: false,
+  color: "",
+  text: "",
+});
+
+
+onMounted(async () => {
+  const user = localStorage.getItem("user");
+  console.log(JSON.parse(user).id);
+    
+  // await getItinerary();
+  // await getItineraryDays();
+  // await getItineraryDaySites();
+  // await getItineraryDayHotel();
+  // console.log(itinerary);
+  if (user !== null) {
+    itinerary.value.userId = JSON.parse(user).id;
+  }
+  console.log(itinerary.value);
+});
+
+async function getItinerary() {
+  await ItineraryServices.getItinerary(route.params.id)
+    .then((response) => {
+      itinerary.value = response.data[0];
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+async function updateItinerary() {
+  await ItineraryServices.updateItinerary(itinerary.value.id, itinerary.value)
+    .then(() => {
+      snackbar.value.value = true;
+      snackbar.value.color = "green";
+      snackbar.value.text = `${itinerary.value.name} updated successfully!`;
+    })
+    .catch((error) => {
+      console.log(error);
+      snackbar.value.value = true;
+      snackbar.value.color = "error";
+      snackbar.value.text = error.response.data.message;
+    });
+  await getItinerary();
+}
+
+async function addItinerary() {
+  await ItineraryServices.addItinerary(itinerary.value).then(
+    () => {
+      snackbar.value.value = true;
+      snackbar.value.color = "green";
+      snackbar.value.text = `${itinerary.value.name} created successfully!`;
+    }
+  )
+  .catch((error) => {
+    console.log(error);
+    snackbar.value.value = true;
+    snackbar.value.color = "error";
+    snackbar.value.text = error.response.data.message;
+  });
+  router.push({ name: "home" });
+}
+
+
+</script>
+
+<template>
+  <v-container>
+    <v-row align="center">
+      <v-col cols="10"
+        ><v-card-title class="pl-0 text-h4 font-weight-bold"
+          >Create Itinerary
+        </v-card-title>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-card class="rounded-lg elevation-5">
+          <v-form ref="form"
+          >
+          <v-card-text>
+            <v-row>
+              <v-col>
+                <v-text-field
+                  v-model="itinerary.name"
+                  label="Name"
+                  required
+                ></v-text-field>
+                <v-text-field
+                  v-model.number="itinerary.duration"
+                  label="Duration"
+                  type="number"
+                ></v-text-field>
+                <v-text-field
+                  v-model.number="itinerary.durationType"
+                  label="Duration Type (M - Months/ d - days/ h - hours)"
+                ></v-text-field>
+              </v-col>
+              <v-col>
+                <v-textarea
+                  v-model="itinerary.description"
+                  rows="10"
+                  label="Description"
+                ></v-textarea>
+              </v-col>
+            </v-row>
+          </v-card-text>
+          <v-card-actions class="pt-0">
+            
+            <v-btn 
+            variant="flat" color="primary"
+            @click="
+              isAddItinerary
+                ? addItinerary()
+                : isEditItinerary
+                ? updateItinerary()
+                : false
+            "
+             >
+             {{
+              isAddItinerary
+                ? "Create Itinerary"
+                : isEditItinerary
+                ? "Update Itinerary"
+                : ""
+            }}
+            </v-btn>
+            
+            <v-spacer></v-spacer>
+          </v-card-actions>
+        </v-form>
+        </v-card>
+      </v-col>
+    </v-row>
+
+    <v-snackbar v-model="snackbar.value" rounded="pill">
+      {{ snackbar.text }}
+
+      <template v-slot:actions>
+        <v-btn :color="snackbar.color" variant="text" @click="closeSnackBar()">
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
+  </v-container>
+</template>
